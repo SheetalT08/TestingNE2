5a26e359775d2e5cad47dd7e241dd15e2ecad694
SheetalT08 <105580267+SheetalT08@users.noreply.github.com>
2022-05-16 20:10:24 +0530

    by Git_sheetal

--- Git_repository/src/com/cjc/Git_demo.java
+++ Git_repository/src/com/cjc/Git_demo.java
@@ -2,7 +2,12 @@ package com.cjc;
 
 public class Git_demo 
 {
-public static void main(String[] args) {
+public static void main(String[] args)
+{
 	System.out.println("hello");
+}
+	public void m1()
+	{
+		System.out.println("m1 method");
 }
 }
